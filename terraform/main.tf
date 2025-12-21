08072ae4f4dab5e1401aac1fc85b48bcd3208be8
AKS144 <abhi144k@gmail.com>
2025-12-21 22:20:42 +0530

    Add missing kube_config variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,34 @@
+# terraform {
+#   required_providers { kubernetes = { source = "hashicorp/kubernetes" } }
+# }
+# provider "kubernetes" {
+#   # Uses KUBECONFIG env var from Jenkins
+# }
+# resource "kubernetes_namespace_v1" "mern_ns" {
+#   metadata { name = "mern-namespace" }
+# }
+
+
 terraform {
-  required_providers { kubernetes = { source = "hashicorp/kubernetes" } }
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
 }
+
+# --- THIS WAS MISSING ---
+variable "kube_config" {
+  type = string
+}
+# ------------------------
+
 provider "kubernetes" {
-  # Uses KUBECONFIG env var from Jenkins
+  config_path = var.kube_config
 }
+
 resource "kubernetes_namespace_v1" "mern_ns" {
-  metadata { name = "mern-namespace" }
+  metadata {
+    name = "mern-namespace"
+  }
 }
